7487381df3dbf6fe566e05c8bf8aaca9eead42e4
cur1576 <cur1576@gmx.de>
2019-03-05 10:58:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mylocation/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mylocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+
+        for(int i=0;i<100;i++){
+            tv.append("Hallo kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll!\n");
+        }
+        tv.append("Welt...");
+    }
+}
